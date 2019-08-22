9e99a2240e936b7c0538aec1b5c30b8e4d96cc59
mzdkzk <mzdkzk@yahoo.co.jp>
2019-08-22 16:43:18 +0900

    曜日を答えるスキル追加

--- web/project/urls.py
+++ web/project/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-# from django.conf import settings
-# from django.conf.urls.static import static
+from . import skills
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', skills.skill_view),
 ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

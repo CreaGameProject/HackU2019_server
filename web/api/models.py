04a09c8395af193a8a0786ea35ff40c1467401ae
mzdkzk <mzdkzk@yahoo.co.jp>
2019-08-29 00:06:59 +0900

    HeartRateモデル追加

--- web/api/models.py
+++ web/api/models.py
@@ -2,6 +2,7 @@ import os
 import uuid
 
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 
 def upload_to(instance: 'Sound', filename: str):
@@ -18,3 +19,12 @@ class AlarmTask(models.Model):
     sound = models.ForeignKey(Sound, on_delete=models.CASCADE)
     sounds_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class HeartRate(models.Model):
+    data = models.TextField(validators=[validate_comma_separated_integer_list])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def split_data(self):
+        return [int(i) for i in self.data.split(',')]
